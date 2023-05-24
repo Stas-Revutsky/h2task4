2918703eb7d098d4bf678acc664d4ec2fdbe6c3e
Admin <stas.revutsky@gmail.com>
2023-05-25 00:01:43 +0300

    Add project files.

---
+++ h2task4/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace h2task4
+{
+
+    internal class Program
+    {
+        struct Dog
+        {
+            public string? name;
+            public string? mark;
+            public int age;
+
+            public override string ToString()
+            {
+                return $"The dog's name is {name}, its mark is {mark} and it is {age} years old";
+            }
+        }
+        static void Main()
+        {
+            Dog dog;
+            Console.WriteLine("Enter dogs name");
+            dog.name = Console.ReadLine();
+            Console.WriteLine("Enter dogs mark");
+            dog.mark = Console.ReadLine();
+            Console.WriteLine("Enter dogs age");
+            dog.age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(dog);
+        }
+    }
+}
